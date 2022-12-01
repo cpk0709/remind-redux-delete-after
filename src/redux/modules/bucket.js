243c9b6c48cd25f3b7912c933405076c34dd1d91
cpk0709 <cpk0709@gmail.com>
2022-12-01 09:13:50 +0900

    reducer 생성

---
+++ src/redux/modules/bucket.js
@@ -0,0 +1,34 @@
+// Actions
+const CREATE = "bucket/CREATE";
+
+const initialState = {
+  list: [
+    { title: "영화관가기", id: 1 },
+    { title: "리액트 공부하기", id: 2 },
+    { title: "여행가기", id: 3 },
+  ],
+};
+
+// Action Creators
+export function createBucket(bucket) {
+  return { type: CREATE, bucket };
+}
+
+// 미들웨어
+// export function getWidget() {
+//   return (dispatch) =>
+//     get("/widget").then((widget) => dispatch(updateWidget(widget)));
+// }
+
+// Reducer
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    // do reducer stuff
+    case "bucket/CREATE": {
+      const new_bucket_list = [...state.list, action.bucket];
+      return { list: new_bucket_list };
+    }
+    default:
+      return state;
+  }
+}
