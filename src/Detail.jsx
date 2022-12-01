a98b89c07e200434e0a6f78e47e34e27880bfe68
cpk0709 <cpk0709@gmail.com>
2022-12-01 09:51:50 +0900

    상세페이지 수정

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,5 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Detail = () => {
-  return <h1>상세페이지</h1>;
+  const params = useParams();
+  const bucketId = params.id;
+  const bucketList = useSelector((state) => state.bucket.list);
+
+  const bucket = bucketList.filter((bucket) => bucket.id === Number(bucketId));
+  //보완필요
+  return (
+    <>
+      <h1>상세페이지</h1>
+      <h3>{bucket[0].title}</h3>
+    </>
+  );
 };
 
 export default Detail;
