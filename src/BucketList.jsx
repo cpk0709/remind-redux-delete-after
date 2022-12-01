d3afa49cc8dd6f201d42234081da30afa9b2311c
cpk0709 <cpk0709@gmail.com>
2022-12-01 09:51:41 +0900

    상세페이지 가기 수정

--- src/BucketList.jsx
+++ src/BucketList.jsx
@@ -4,15 +4,15 @@ import { useSelector } from "react-redux";
 const BucketList = () => {
   const my_list = useSelector((state) => state.bucket.list);
   const history = useHistory();
-  const handleMoveToDetail = () => {
-    history.push("/detail");
+  const handleMoveToDetail = (id) => {
+    history.push(`/detail/${id}`);
   };
 
   return (
     <div>
       {my_list.map((todo) => {
         return (
-          <h2 key={todo.id} onClick={handleMoveToDetail}>
+          <h2 key={todo.id} onClick={() => handleMoveToDetail(todo.id)}>
             {todo.title}
           </h2>
         );
